91751e2caff1f9aab10658ed925c0f1019dfd513
Anna <annaersh1994@gmail.com>
2021-08-25 13:56:58 +0300

    Initial Commit

---
+++ task10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task10
+//
+//  Created by Anna Ershova on 25.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
