18d4ba06869b75f8fbb50532e5a7c73d7da08504
Anna <annaersh1994@gmail.com>
2021-08-25 17:23:32 +0300

    init: begin task

---
+++ task10/NewGameVC.swift
@@ -0,0 +1,29 @@
+//
+//  NewGameVC.swift
+//  task10
+//
+//  Created by Anna Ershova on 25.08.2021.
+//
+
+import UIKit
+
+class NewGameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.backgroundColor = UIColor(red: 0.136, green: 0.136, blue: 0.138, alpha: 1).cgColor
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
